061df68ac7c86fddae939a28c907187686323b77
anapaulabr <48135728+anapaulabr@users.noreply.github.com>
2019-03-01 16:05:03 -0300

    Add files via upload

---
+++ exercicio_1_soma.php
@@ -0,0 +1,6 @@
+<?php
+
+$numero1=3;
+$numero2=4;
+$soma=$numero1+$numero2;
+print $soma;
---
+++ exercicio_2_media.php
@@ -0,0 +1,10 @@
+<?php
+
+$nota1 = 10;
+$nota2 = 8;
+$nota3 = 9;
+$nota4 = 10;
+
+$media = ($nota1 + $nota2 + $nota3 + $nota4)/4;
+
+print $media;
